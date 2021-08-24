268d0f19a1cf752c3a9a529334f932f9f1bbb75b
Connor <connorjameslow@gmail.com>
2021-08-23 21:07:08 -0700

    Fix some stuff

--- Portfolio2021/publish.ps1
+++ Portfolio2021/publish.ps1
@@ -1,9 +1,9 @@
 Write-Output "dotnet publish:"
-dotnet publish -c Release -o .\publish\wwwroot
+dotnet publish -c Release -o .\bin\Publish\
 
 Write-Output "push gh-pages"
-Set-Location .\publish\wwwroot
+Set-Location .\bin\Publish\wwwroot
 git add .
 git commit -m "via publish script"
-git push
-Set-Location ..\..
+git push origin gh-pages
+Set-Location ..\..\..
